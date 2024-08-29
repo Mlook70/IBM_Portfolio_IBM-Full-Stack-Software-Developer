6ba13cb1445ab3362408ce39c36a945ba6afdbac
Mlook70 <Abdulmalek.akel@gmail.com>
2024-08-29 09:48:02 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('.recommendation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Recommendation submitted! Thank you.');
+    // You can add functionality to handle form submission here
+});
